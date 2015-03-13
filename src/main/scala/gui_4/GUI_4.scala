3c84a03d817a730dabc87a4f27b7f3bdaa5b08fe
timnix91 <timothy.nix@usma.edu>
2015-03-13 15:44:16 -0400

    Changed views

--- src/main/scala/gui_4/GUI_4.scala
+++ src/main/scala/gui_4/GUI_4.scala
@@ -8,8 +8,9 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val model = new Model
-    val view = new TextView
-    //val view = new PanelView
+    //val view = new TextView
+    // we switched views
+    val view = new PanelView
     val controller = new Controller(view, model)
     view.init(controller)
   
